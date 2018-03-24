588a58decdbc6ff0ed79d206ef6e5341bb81c649
Samuel Ballan <sgb4622@gmail.com>
2018-03-24 19:00:16 -0400

    post view

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe "Creation" do 
+		before do
+			@post = Post.create(created_at: Date.today, text: "My first Post")
+		end
+
+		it "can be created" do
+			expect(@post).to be_valid
+		end
+
+		it "cannot be created without a `created_at` and `text`" do
+			@post.created_at = nil
+			@post.text = nil
+			expect(@post).to_not be_valid
+		end
+	end
 end
